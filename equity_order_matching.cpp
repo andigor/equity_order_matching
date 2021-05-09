42d8e1ffa2b6319697d2eaf94f1289610882f531
Andrii Motsok <andrii.motsok@nl.abb.com>
2021-05-09 11:47:00 +0200

    Buy queue.

--- equity_order_matching.cpp
+++ equity_order_matching.cpp
@@ -3,12 +3,16 @@
 #include <map>
 #include <cassert>
 
-
+struct order_data_key
+{
+  uint32_t m_time;
+  float m_price;
+};
 
 class order_data
 {
 public:
-  order_data(uint64_t id, int tim, const std::string& nam, float price, uint64_t q)
+  order_data(uint64_t id, uint32_t tim, const std::string& nam, float price, uint64_t q)
   : m_id(id)
   , m_time(tim)
   , m_symbol(nam)
@@ -22,6 +26,7 @@ public:
   std::string get_symbol() const { return m_symbol; }
   float get_price()        const { return m_price; }
   uint64_t get_q()         const { return m_q; }
+
   void reduce_q(uint64_t q)
   {
     assert(q <= m_q);
@@ -31,9 +36,13 @@ public:
   {
     m_q = 0;
   }
+  order_data_key get_key() const
+  {
+    return { m_time, m_price };
+  }
 private:
   uint64_t m_id;
-  int m_time;
+  uint32_t m_time;
   std::string m_symbol;
   float m_price;
   uint64_t m_q;
@@ -68,12 +77,75 @@ public:
   {
     m_order_data.reset_q();
   }
+  float get_price() const
+  {
+    return m_order_data.get_price();
+  }
+  uint32_t get_time() const
+  {
+    return m_order_data.get_time();
+  }
+  order_data_key get_key() const
+  {
+    return m_order_data.get_key();
+  }
+  uint64_t get_id() const
+  {
+    return m_order_data.get_id();
+  }
 private:
   order_data m_order_data;
 };
 
-using limit_order_sell = order<order_side::sell>;
-using limit_order_buy  = order<order_side::buy>;
+class limit_order_sell : public order<order_side::sell>
+{
+public:
+  limit_order_sell(const order_data& od)
+    :order(od)
+  {
+
+  }
+};
+
+class limit_order_buy : public order<order_side::buy>
+{
+public:
+  limit_order_buy(const order_data& od)
+    :order(od)
+  {
+
+  }
+};
+
+
+struct limit_order_buy_less
+{
+  bool operator()(const order_data_key& k1, const order_data_key& k2) const
+  {
+    if (k1.m_price > k2.m_price) {
+      return false;
+    }
+    if (k1.m_price < k2.m_price) {
+      return true;
+    }
+    return k1.m_time < k2.m_time;
+  }
+};
+
+struct limit_order_sell_less
+{
+  bool operator()(const order_data_key& k1, const order_data_key& k2) const
+  {
+    if (k1.m_price < k2.m_price) {
+      return true;
+    }
+    if (k1.m_price > k2.m_price) {
+      return false;
+    }
+
+    return k1.m_time < k2.m_time;
+  }
+};
 
 struct match_result
 {
@@ -90,20 +162,7 @@ match_result make_sell_match_result(uint64_t matched_sell_count)
 {
   return { 0, matched_sell_count };
 }
-//class order_type_market
-//{
-//
-//};
-//
-//class order_type_limit
-//{
-//
-//};
-//
-//class order_type_limit_ioc
-//{
-//
-//};
+
 
 match_result match_limit_orders(limit_order_buy& buy, limit_order_sell& sell)
 {
@@ -187,9 +246,84 @@ void basic_limit_orders_matching_tests()
   }
 }
 
+
+template <class Ord, class Cmp>
+class limit_order_queue : public std::multimap<order_data_key, Ord, Cmp>
+{
+public:
+  void put_order(const Ord& o)
+  {
+    this->insert(std::make_pair(o.get_key(), o));
+  }
+};
+
+class limit_order_buy_queue: public limit_order_queue<limit_order_buy, limit_order_buy_less>
+{
+public:
+};
+
+class limit_order_sell_queue: public limit_order_queue<limit_order_sell, limit_order_sell_less>
+{
+public:
+};
+
+
+void basic_buy_queue_tests()
+{
+  {
+    limit_order_buy_queue bq;
+    limit_order_buy b1({ 1, 1, "n", 1.0f, 1 });
+    bq.put_order(b1);
+
+    limit_order_buy b2({ 2, 1, "n", 1.0f, 1 });
+    bq.put_order(b2);
+
+    assert(bq.size() == 2);
+    {
+      auto iter = bq.begin();
+      assert(iter->second.get_id() == 1);
+      ++iter;
+      assert(iter->second.get_id() == 2);
+    }
+  }
+  {
+    limit_order_buy_queue bq;
+    limit_order_buy b1({ 1, 2, "n", 1.0f, 1 });
+    bq.put_order(b1);
+
+    limit_order_buy b2({ 2, 1, "n", 1.0f, 1 });
+    bq.put_order(b2);
+
+    assert(bq.size() == 2);
+    {
+      auto iter = bq.begin();
+      assert(iter->second.get_id() == 2);
+      ++iter;
+      assert(iter->second.get_id() == 1);
+    }
+  }
+
+  {
+    limit_order_buy_queue bq;
+    limit_order_buy b1({ 1, 1, "n", 2.0f, 1 });
+    bq.put_order(b1);
+
+    limit_order_buy b2({ 2, 1, "n", 1.0f, 1 });
+    bq.put_order(b2);
+
+    assert(bq.size() == 2);
+    {
+      auto iter = bq.begin();
+      assert(iter->second.get_id() == 2);
+      ++iter;
+      assert(iter->second.get_id() == 1);
+    }
+  }
+}
+
 int main()
 {
 
   basic_limit_orders_matching_tests();
-
+  basic_buy_queue_tests();
 }
